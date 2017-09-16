ead84e3790b76f79d8ba45591e4946e14e25476d
unknown <568969859@qq.com>
2017-09-16 11:30:52 +0800

    login

--- index/src/components/login/Login.vue
+++ index/src/components/login/Login.vue
@@ -1,6 +1,13 @@
 <template>
-    <div>
-
+    <div class="rome">
+        <div class="login"></div>
+        <div class="welcome">
+            <div class="wel">WELCOMETO</div>
+            <div class="order">ORDER</div>
+        </div>
+        <div class="button"></div>
+        <div class="else"></div>
+        <div class="sina"></div>
     </div>
 </template>
 
@@ -18,5 +25,36 @@
 </script>
 
 <style scoped>
-
+    .rome{
+        height:100vh;
+    }
+    .login{
+        background:url('/static/img/ybl_04.png') top left/cover;
+        width:100% ;
+        height:100%;
+        position:absolute;
+        top:0;
+        left:0;
+    }
+    .welcome{
+        position: absolute;
+        top:2rem;
+        text-align: center;
+        width:100%;
+    }
+    .wel{
+        text-align: center;
+        font-size:.28rem;
+        color: #FF9313;
+        font-weight: bold;
+        letter-spacing: .1rem;
+    }
+    .order{
+        text-align: center;
+        font-size:.22rem;
+        color: #FF9313;
+        font-weight: bold;
+        letter-spacing: .42rem;
+        margin-left:.27rem;
+    }
 </style>
--- index/src/router/index.js
+++ index/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
 import w_guide from '../components/guild/w_guide.vue'
+import login from '../components/login'
 
 Vue.use(Router)
 
@@ -11,6 +12,11 @@ export default new Router({
       path: '/',
       name: 'guide',
       component: w_guide
-    }
+    },
+      {
+        path:'/login',
+          name:'login',
+          component:login
+      }
   ]
 })
