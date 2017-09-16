0e903872b71c9f0407c5185e6f3d1d9513dda4ca
innoccent <867913275@qq.com>
2017-09-16 11:48:47 +0800

    'main.js

--- index/src/components/guild/w_guide.vue
+++ index/src/components/guild/w_guide.vue
@@ -1,11 +1,37 @@
 <template>
-
+    <swiper :options="swiperOption" ref="mySwiper">
+        <!-- slides -->
+        <swiper-slide>I'm Slide 1</swiper-slide>
+        <swiper-slide>I'm Slide 2</swiper-slide>
+        <swiper-slide>I'm Slide 3</swiper-slide>
+        <!-- Optional controls -->
+        <div class="swiper-pagination"  slot="pagination"></div>
+        <div class="swiper-button-prev" slot="button-prev"></div>
+        <div class="swiper-button-next" slot="button-next"></div>
+        <div class="swiper-scrollbar"   slot="scrollbar"></div>
+    </swiper>
 </template>
 
 <script>
-
+    import { swiper, swiperSlide } from 'vue-awesome-swiper'
     export default {
-        name: 'guide'
+        name: 'carrousel',
+        data() {
+            return {
+                swiperOption: {
+                    // notNextTick是一个组件自有属性，如果notNextTick设置为true，组件则不会通过NextTick来实例化swiper，也就意味着你可以在第一时间获取到swiper对象，假如你需要刚加载遍使用获取swiper对象来做什么事，那么这个属性一定要是true
+                    notNextTick: true,
+                    // swiper configs 所有的配置同swiper官方api配置
+                    autoplay: 3000,
+                    prevButton:'.swiper-button-prev',
+                    nextButton:'.swiper-button-next',
+                }
+            }
+        },
+        components: {
+            swiper,
+            swiperSlide
+        },
     }
 </script>
 
