fbe0ed25f7b0a5941808728a4d6afc9318fc054e
innoccent <867913275@qq.com>
2017-09-16 11:37:36 +0800

    'guide'

--- index/src/router/index.js
+++ index/src/router/index.js
@@ -2,7 +2,11 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
 import w_guide from '../components/guild/w_guide.vue'
+<<<<<<< Updated upstream
 import login from '../components/login/Login'
+=======
+import p_guide from '../components/guild/w_guide.vue'
+>>>>>>> Stashed changes
 
 Vue.use(Router)
 
@@ -14,9 +18,15 @@ export default new Router({
       component: w_guide
     },
       {
+<<<<<<< Updated upstream
         path:'/login',
           name:'login',
           component:login
+=======
+        path:'/w',
+        name:'w',
+        component:p_guide
+>>>>>>> Stashed changes
       }
   ]
 })
