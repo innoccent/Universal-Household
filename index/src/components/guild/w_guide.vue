7eb7a0435b9a27886ba290e6222c637b6893e0d6
innoccent <867913275@qq.com>
2017-09-16 11:13:21 +0800

    guide页

---
+++ index/src/components/guild/w_guide.vue
@@ -0,0 +1,14 @@
+<template>
+
+</template>
+
+<script>
+    export default {
+        name: 'guide'
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
