e2d9239b5f3967fb0b4f9b672218a235b95820dd
innoccent <867913275@qq.com>
2017-09-16 16:34:57 +0800

    'guide'

--- index/src/components/guild/w_guide.vue
+++ index/src/components/guild/w_guide.vue
@@ -1,17 +1,23 @@
 <template>
-    <div class="guide">
+    <div class="guide" @touchend="touch">
         <swiper :options="swiperOption" ref="mySwiperA" class="swip">
             <!-- 幻灯内容 -->
             <swiper-slide class="slide"><img src="/static/img/leader_03.png" alt=""></swiper-slide>
             <swiper-slide class="slide"><img src="/static/img/leader_05.png" alt=""></swiper-slide>
             <swiper-slide class="slide"><img src="/static/img/leader_10.png" alt=""></swiper-slide>
         </swiper>
+        <ul class="page">
+            <li :class="{active:this.active==0}"></li>
+            <li :class="{active:this.active==1}"></li>
+            <li :class="{active:this.active==2}"></li>
+        </ul>
     </div>
 </template>
 <script>
     export default{
         data(){
             return {
+                active:0,
                 swiperOption: {
                     // 所有配置均为可选（同Swiper配置）
                     notNextTick: true,
@@ -36,9 +42,11 @@
             swiper() {
                 return this.$refs.mySwiperA.swiper
             },
-            active(){
-                return this.swiperOption.active;
-            }
+        },
+        methods:{
+          touch(){
+              this.active=this.swiper.realIndex;
+          }
         },
         mounted() {
 
@@ -66,5 +74,28 @@
     .slide > img{
         width:100%;
         height: 100%;
+    }
+    .page{
+        width:100%;
+        height:0.04rem;
+        position: absolute;
+        bottom:0.39rem;
+        left:0;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+    .page > li{
+        width:0.1rem;
+        height:100%;
+        background: #ffcc85;
+        margin:0 0.125rem;
+        transition: background .3s linear;
+        border-radius: 0.02rem;
+    }
+    .page > li.active{
+        width:0.15rem;
+        height:100%;
+        background: #e03737;
     }
 </style>
