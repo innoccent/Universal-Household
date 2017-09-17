a8dd97bbc60a46b0a2a5470db0ab386da3003415
Nancy0123 <1065997154@qq.com>
2017-09-17 12:44:11 +0800

    'apply'

--- index/src/router/index.js
+++ index/src/router/index.js
@@ -1,26 +1,21 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
-<<<<<<< HEAD
 import GoodsDetails from '../components/GoodsDetails.vue'
 import OnlinePay from '../components/OnlinePay.vue'
-=======
 import w_guide from '../components/guild/w_guide.vue'
 import login from '../components/login/Login'
 import yloginin from '../components/login/yloginin.vue'
 import yregister from '../components/login/yregister.vue'
 import p_guide from '../components/guild/w_guide.vue'
->>>>>>> 332815c1808715e88b07808182f030976f9a0bb0
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-<<<<<<< HEAD
-      name: 'Hello',
-      component: Hello
+        path: '/',
+        name: 'guide',
+        component: w_guide
     },
 
       {
@@ -31,10 +26,7 @@ export default new Router({
       {
           path: '/onlinepay',
           name: 'onlinepay',
-          component: OnlinePay
-=======
-      name: 'guide',
-      component: w_guide
+          component:OnlinePay
     },
       {
         path:'/login',
@@ -55,7 +47,6 @@ export default new Router({
         path:'/w',
         name:'w',
         component:p_guide
->>>>>>> 332815c1808715e88b07808182f030976f9a0bb0
       }
   ]
 })
